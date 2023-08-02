1036f71097be9d98d022c668f5ca41062c32acd9
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-02 20:09:40 +0300

    @hotjar/browser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,15 @@ import Header from '../cmps/Header'
 import Footer from '../cmps/Footer'
 import { Analytics } from '@vercel/analytics/react'
 
-import { hotjar } from 'react-hotjar'
+import Hotjar from '@hotjar/browser'
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if (hotjar.initialized()) {
-      hotjar.identify(process.env.HOTJAR_ID as string, {
-        userProperty: process.env.HOTJAR_SNIPPET_VERSION as string,
-      })
-    }
+    const siteId = Number(process.env.NEXT_PUBLIC_HOTJAR_ID)
+    const hotjarVersion = Number(process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION)
+
+    Hotjar.init(siteId, hotjarVersion)
   }, [])
 
   return (
