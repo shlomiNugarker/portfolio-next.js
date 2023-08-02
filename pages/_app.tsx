6addbcaa1ce13196ee0d43f910b5872b023c1ed9
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-02 21:38:01 +0300

    tracking code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,46 @@ export default function App({ Component, pageProps }: AppProps) {
     const siteId = Number(process.env.NEXT_PUBLIC_HOTJAR_ID)
     const hotjarVersion = Number(process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION)
 
-    Hotjar.init(siteId, hotjarVersion)
+    Hotjar.init(siteId, hotjarVersion, {
+      debug: true,
+    })
+    //
+    ;(function (
+      h: any,
+      o: any,
+      t: any,
+      j: any,
+      a?: { appendChild: (v: any) => any },
+      r?: { async?: number; src?: any }
+    ) {
+      console.log('rrrrr')
+
+      h.hj =
+        h.hj ||
+        function () {
+          ;(h.hj.q = h.hj.q || []).push(arguments)
+        }
+      h._hjSettings = { hjid: 3598136, hjsv: 6 }
+      a = o.getElementsByTagName('head')[0]
+      r = o.createElement('script')
+      if (!!r) {
+        r.async = 1
+        r.src = t + siteId + j + hotjarVersion
+        a?.appendChild(r)
+      }
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+
+    const actionName = 'error'
+    Hotjar.event(actionName)
+
+    const userId = 'abc_123'
+    const firstName = 'John'
+    const favoriteColor = 'blue'
+
+    Hotjar.identify(userId, {
+      first_name: firstName,
+      color: favoriteColor,
+    })
   }, [])
 
   return (
