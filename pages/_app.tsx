7f0c61808fb5787a219b94972f42063a7cfdaec7
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-04 13:06:12 +0300

    minor change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,36 +7,34 @@ import { Analytics } from '@vercel/analytics/react'
 
 import Hotjar from '@hotjar/browser'
 import { useEffect } from 'react'
-import Script from 'next/script'
+
+const siteId = Number(process.env.NEXT_PUBLIC_HOTJAR_ID)
+const hotjarVersion = Number(process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION)
 
 export default function App({ Component, pageProps }: AppProps) {
-  const siteId = Number(process.env.NEXT_PUBLIC_HOTJAR_ID)
-  const hotjarVersion = Number(process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION)
-
   useEffect(() => {
     Hotjar.init(siteId, hotjarVersion, {
       debug: true,
     })
 
-    const actionName = 'error'
-    Hotjar.event(actionName)
+    // const actionName = 'error'
+    // Hotjar.event(actionName)
 
-    const userId = 'abc_123'
-    const firstName = 'John'
-    const favoriteColor = 'blue'
+    // const userId = 'abc_123'
+    // const firstName = 'John'
+    // const favoriteColor = 'blue'
 
-    Hotjar.identify(userId, {
-      first_name: firstName,
-      color: favoriteColor,
-    })
-  }, [hotjarVersion, siteId])
+    // Hotjar.identify(userId, {
+    //   first_name: firstName,
+    //   color: favoriteColor,
+    // })
+  }, [])
 
   return (
     <>
       <Header />
       <Component {...pageProps} />
       <Footer />
-
       <Analytics />
     </>
   )
