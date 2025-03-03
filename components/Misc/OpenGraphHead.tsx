bb9c61c7249a3024736ef94dd45c00d8a85c6cdb
Shlomi Nugarker <shlomin1231@gmail.com>
2025-03-03 10:39:42 +0200

    feat(OpenGraphHead, localization): implement dynamic Open Graph metadata and add English and Hebrew translations

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,57 +1,49 @@
 import Head from 'next/head'
+import { useTranslation } from 'next-i18next'
 
-const OpenGraphHead = () => (
-  <Head>
-    {/* General Metadata */}
-    <meta charSet="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <title>Shlomi Nugarker | Fullstack Developer</title>
-    <meta
-      name="description"
-      content="My personal solace place on web-earth. Explore my work and projects."
-    />
-    <meta name="author" content="Shlomi Nugarker" />
+const OpenGraphHead = () => {
+  const { t } = useTranslation('common')
 
-    {/* Open Graph / Facebook */}
-    <meta property="og:title" content="Shlomi Nugarker | Fullstack Developer" />
-    <meta property="og:site_name" content="Shlomi Nugarker | Portfolio" />
-    <meta property="og:url" content="https://shlomi-nugarker.vercel.app/" />
-    <meta
-      property="og:description"
-      content="Howdy! I am Shlomi Nugarker, a passionate Fullstack Developer. Explore my work here!"
-    />
-    <meta property="og:type" content="profile" />
-    <meta
-      property="og:image"
-      content="https://res.cloudinary.com/duajg3ah1/image/upload/v1660763357/shlomiN_mewit4.jpg"
-    />
-    <meta
-      property="og:image:alt"
-      content="Shlomi Nugarker - Fullstack Developer"
-    />
+  const ogTitle = t('opengraph.title')
+  const ogDescription = t('opengraph.description')
 
-    {/* Twitter Card */}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta
-      name="twitter:title"
-      content="Shlomi Nugarker | Fullstack Developer"
-    />
-    <meta
-      name="twitter:description"
-      content="Discover my projects, blog, and skills in Fullstack Development."
-    />
-    <meta
-      name="twitter:image"
-      content="https://res.cloudinary.com/duajg3ah1/image/upload/v1660763357/shlomiN_mewit4.jpg"
-    />
-    <meta name="twitter:creator" content="@YourTwitterHandle" />
+  return (
+    <Head>
+      {/* General Metadata */}
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <title>{ogTitle}</title>
+      <meta name="description" content={ogDescription} />
 
-    {/* Favicon */}
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-)
+      {/* Open Graph / Facebook */}
+      <meta property="og:title" content={ogTitle} />
+      <meta property="og:site_name" content={ogTitle} />
+      <meta property="og:url" content="https://shlomi-nugarker.vercel.app/" />
+      <meta property="og:description" content={ogDescription} />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/duajg3ah1/image/upload/v1740965833/hqebkt3feaumbj5nma7o.png"
+      />
+      <meta property="og:image:alt" content={ogTitle} />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={ogTitle} />
+      <meta name="twitter:description" content={ogDescription} />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/duajg3ah1/image/upload/v1740965833/hqebkt3feaumbj5nma7o.png"
+      />
+      <meta name="twitter:creator" content="@shlomi_nugarker" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
 
 export default OpenGraphHead
