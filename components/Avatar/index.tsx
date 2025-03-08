3c86fb7e1bd2ba33d26314073e33afff89df7f72
Shlomi Nugarker <shlomin1231@gmail.com>
2025-03-09 00:02:06 +0200

    refactor: adjust Avatar component dimensions for improved consistency

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -43,7 +43,7 @@ const Avatar = () => {
     <AnimatePresence>
       <MotionBox
         id="klAvatar"
-        boxSize={{ base: '400px', md: '400px' }}
+        boxSize={{ base: '300px', md: '300px' }}
         padding={{ base: 8 }}
         marginBottom={{ base: 10, md: 0, lg: 0 }}
         initial="initial"
@@ -55,8 +55,8 @@ const Avatar = () => {
           src={imgAvatar}
           alt="Shlomi's Avatar"
           borderRadius="full"
-          htmlWidth="400"
-          htmlHeight="400"
+          htmlWidth="300"
+          htmlHeight="300"
           margin="auto"
           fallback={
             (
