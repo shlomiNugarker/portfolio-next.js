31808067e842860801f23c6262de5b4ca0594b30
Shlomi Nugarker <shlomin1231@gmail.com>
2025-03-15 21:10:09 +0200

    refactor: improve locale handling and add security headers in document structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,37 +8,76 @@ import Document, {
 } from 'next/document'
 import { ReactNode } from 'react'
 
-type MyDocumentProps = DocumentInitialProps & {
-  locale: string
+// Constants
+const SUPPORTED_LOCALES = ['en', 'he', 'ar', 'ru', 'fr', 'es', 'de', 'hi'] as const
+const RTL_LOCALES = ['he', 'ar'] as const
+const DEFAULT_LOCALE = 'en'
+
+type SupportedLocale = typeof SUPPORTED_LOCALES[number]
+
+interface MyDocumentProps extends DocumentInitialProps {
+  locale: SupportedLocale
 }
 
 class MyDocument extends Document<MyDocumentProps> {
   static async getInitialProps(ctx: DocumentContext): Promise<MyDocumentProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    let locale = 'en'
+    try {
+      const initialProps = await Document.getInitialProps(ctx)
+      
+      // Extract locale from URL
+      const locale = MyDocument.extractLocaleFromContext(ctx)
 
-    if (ctx.query && ctx.query.asPath) {
-      locale = Array.isArray(ctx.query.asPath)
-        ? ctx.query.asPath[0].split('/')[1]
-        : ctx.query.asPath.split('/')[1]
-    } else if (ctx.req && ctx.req.url) {
-      const segments = ctx.req.url.split('/')
-      if (segments[1]) {
-        locale = segments[1]
+      return {
+        ...initialProps,
+        locale: MyDocument.validateLocale(locale),
+      }
+    } catch (error) {
+      console.error('Error in document getInitialProps:', error)
+      return {
+        ...await Document.getInitialProps(ctx),
+        locale: DEFAULT_LOCALE,
       }
     }
+  }
 
-    return { ...initialProps, locale }
+  private static extractLocaleFromContext(ctx: DocumentContext): string {
+    if (ctx.query?.asPath) {
+      const path = Array.isArray(ctx.query.asPath)
+        ? ctx.query.asPath[0]
+        : ctx.query.asPath
+      return path.split('/')[1] || DEFAULT_LOCALE
+    }
+
+    if (ctx.req?.url) {
+      return ctx.req.url.split('/')[1] || DEFAULT_LOCALE
+    }
+
+    return DEFAULT_LOCALE
+  }
+
+  private static validateLocale(locale: string): SupportedLocale {
+    return SUPPORTED_LOCALES.includes(locale as SupportedLocale)
+      ? (locale as SupportedLocale)
+      : DEFAULT_LOCALE
+  }
+
+  private static isRTL(locale: SupportedLocale): boolean {
+    return RTL_LOCALES.includes(locale as typeof RTL_LOCALES[number])
   }
 
   render(): ReactNode {
     const { locale } = this.props
-    const dir = ['he', 'ar'].includes(locale) ? 'rtl' : 'ltr'
+    const dir = MyDocument.isRTL(locale) ? 'rtl' : 'ltr'
 
     return (
       <Html lang={locale} dir={dir}>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* Preload fonts */}
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="anonymous"
+          />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
@@ -47,7 +86,21 @@ class MyDocument extends Document<MyDocumentProps> {
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600&display=swap"
             rel="stylesheet"
+            crossOrigin="anonymous"
           />
+          
+          {/* Security headers */}
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="format-detection" content="date=no" />
+          <meta name="format-detection" content="address=no" />
+          <meta name="format-detection" content="email=no" />
+          
+          {/* PWA related tags */}
+          <meta name="application-name" content="Shlomi's Portfolio" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <body>
           <Main />
